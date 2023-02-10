af27970e3a86ed6d8d2337bb0adcd113613c9fd7
MiyuYokoyama0109 <miyu.y@latona.io>
2023-02-10 10:06:04 +0900

    release

---
+++ README.md
@@ -0,0 +1,14 @@
+# data-platform-code-conversion-sql
+data-platform-code-conversion-sql は、データ連携基盤において、コード変換データを維持管理するSQLテーブルを作成するためのレポジトリです。  
+
+## 前提条件  
+data-platform-code-conversion-sql は、データ連携にあたり、API を利用し、本レポジトリ の sql 設定ファイルの内容は、下記 URL の API 仕様を前提としています。  
+https://api.XXXXXXXX.com/api/OP_API_XXXXXXX_XXX/overview  
+
+## sqlの設定ファイル
+data-platform-code-conversion-sql には、sqlの設定ファイルとして以下のsqlファイルが含まれています。  
+
+* data-platform-code-conversion-sql-code-conversion-data.sql （データ連携基盤 コード変換 - コード変換データ）
+
+## MySQLのセットアップ / Kubernetesの設定 / SQLテーブルの作成方法
+MySQLのセットアップ / Kubernetesの設定 / 具体的なSQLテーブルの作成方法、については、[mysql-kube](https://github.com/latonaio/mysql-kube)を参照ください。
---
+++ data-platform-code-conversion-sql-code-conversion-data.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `data_platform_code_conversion_code_conversion_data`
+(
+    `CodeConversionID`          int(4) NOT NULL,
+    `SystemConvertTo`           varchar(10) NOT NULL,
+    `SystemConvertFrom`         varchar(10) NOT NULL,
+    `LabelConvertTo`            varchar(50) NOT NULL,
+    `LabelConvertFrom`          varchar(50) NOT NULL,
+    `CodeConvertFromInt`        int(20) DEFAULT NULL,
+    `CodeConvertFromFloat`      float(20) DEFAULT NULL,
+    `CodeConvertFromString`     varchar(100) DEFAULT NULL,
+    `CodeConvertToInt`          int(20) DEFAULT NULL,
+    `CodeConvertToFloat`        float(20) DEFAULT NULL,
+    `CodeConvertToString`       varchar(100) DEFAULT NULL,
+    `BusinessPartner`           int(12) NOT NULL,
+  
+    PRIMARY KEY (`CodeConversionID`, `SystemConvertTo`, `SystemConvertFrom`, `LabelConvertTo`, `LabelConvertFrom`, `BusinessPartner`),
+    CONSTRAINT `DataPlatformCodeConversionCodeConversionDataBusinessPartner_fk` FOREIGN KEY (`BusinessPartner`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`)
+
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
